86a3b1bc125002490899a44b7044811703f939ab
teschner <julian.teschner@adesso.de>
2021-11-18 07:59:22 +0100

    ebdubg cadd description

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     // data: function() {}
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'Some socks'
         }
     }
 })
